ae2d668ece13391aa9e200780420f233a194ef95
Sinna <sinna27@naver.com>
2024-06-09 04:11:18 +0900

    *.log *.pot *.pyc __pycache__/ local_settings.py db.sqlite3 db.sqlite3-journal media Please enter the commit message for your changes. Lines starting

---
+++ board/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone  
+
+class Board(models.Model):
+    title = models.CharField(max_length=50)
+    wirter = models.CharField(max_length=50)
+    content = models.TextField()
+    regdate = models.DateTimeField(auto_now=timezone.now)
+    readcount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '%s. %s(%d)' % (self.title, self.wirter, self.readcount)
+    
+    def incrementReadCount(self):
+        self.readcount +=1
+        self.save()
+        
